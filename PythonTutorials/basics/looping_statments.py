2219899a1d77fde94deea577846db92954b8e221
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-04-03 10:06:42 +0530

    03-04-25: Loop control statements

--- PythonTutorials/basics/looping_statments.py
+++ PythonTutorials/basics/looping_statments.py
@@ -11,17 +11,38 @@ Types:
     b. while condition >> while block
     c. increment/ decrement
 2. for loop
-'''
-count=0
 
-while count<5:
+Loop control statements:
+1. break- terminate/ stop the execution of loop
+2. continue- it skips the execution of loop for once
+
+pass statement: doesn't perform any operations. It is added to make program syntactically correct.
+'''
+'''
+count=2.1
+
+while count>0:
     # print("count:",count)
     # print("count<5: ",count<5)
     print("Hello world!")
     # count=count+1
-    count+=1 # increment operator: +=; decrement operator:-=
+    count-=1 # increment operator: +=; decrement operator:-=
 
 # print("count:",count)
 # print("count<5: ",count<5)
+'''
+'''
+while True:
+    pass
+print("The end")
+'''
 
+count=0
+while count<=100:
+    if count == 50:
+        count+=1
+        continue
+    print(count)
+    count+=1
+    
 
--- PythonTutorials/basics/user_input.py
+++ PythonTutorials/basics/user_input.py
@@ -3,12 +3,11 @@ Created on 29-Mar-2025
 
 @author: Vivek
 '''
-a=input("Enter a number:")
-b=input("Enter another number:")
-c=a+b
-# print(f"Addition of {a} and {b} is " , a+b )
-# print(type(a))
-# print(type(b))
-d=float(a)
-e=float(b)
-print(f"Addition of {d} and {e} is " , d+e )
+while True:
+    a=float(input("Enter a number:"))
+    b=float(input("Enter another number:"))
+    c=a+b
+    print(f"Addition of {a} and {b} is " , a+b )
+    d=input("Do you want to continue?(Y/N):")
+    if d == 'N':
+        break
