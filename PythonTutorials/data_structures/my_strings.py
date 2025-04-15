26165c3f89ef6c1d74aeb83b742422e4f4ad734f
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-04-15 09:53:34 +0530

    15-04-25: Strings

--- PythonTutorials/data_structures/my_strings.py
+++ PythonTutorials/data_structures/my_strings.py
@@ -9,10 +9,11 @@ letters: Characters in python
 
 Def: String is a character/ collection of characters
 
-
+Leading spaces: beginning
+Trailing spaces: end
 '''
 
-name = "vivek"
+name = "VivekNagaraju"
 
 print("name:", name)
 print(type(name))
@@ -40,9 +41,7 @@ print(name[1:4])
 print(type(name[1:4])) 
 
 address="""iQuest,
-
 Hebbal Industrial area,
-
 Mysuru.
 """
 print(address)
@@ -55,3 +54,35 @@ modified_name=name.replace('v', 'z')
 print("name:",name)
 print("modified_name:", modified_name)
 
+print(name.capitalize())
+print(name.strip().capitalize())
+
+print(address.casefold())
+
+print(name.count('v'))
+
+print(name.upper())
+
+print(name.find('z'))
+
+# print(name.index('z')) #Raises ValueError when the substring is not found.
+
+print(name.isalnum())
+print(name.casefold())
+print(name.lower())
+
+print(name.split())
+
+name_list=["My", "Name", "is", "Vivek"]
+
+name_sentence='_'.join(name_list)
+print(name_sentence)
+
+print(len(name))
+
+print(name.isalpha())
+
+print(name.isnumeric())
+
+print(name.isalnum())
+    
