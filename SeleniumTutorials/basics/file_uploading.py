bb83fa5c16635b6e2a15da1bbdd6d57519dc9d24
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-05-19 11:56:37 +0530

    file uploading

---
+++ SeleniumTutorials/basics/file_uploading.py
@@ -0,0 +1,24 @@
+'''
+Created on 19-May-2025
+
+@author: admin
+'''
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+'''1. Launching the chrome browser'''
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options)
+# driver = Chrome()
+
+'''2. Navigating to practice site'''
+driver.get('https://testautomationpractice.blogspot.com/')
+
+'''3. Upload single file'''
+single_file_input = driver.find_element(By.ID, 'singleFileInput')
+single_file_input.send_keys('C:\\Users\\admin\\Downloads\\info.txt')
+
+single_upload_btn = driver.find_element(By.XPATH, "//*[@id=\"singleFileForm\"]/button")
+single_upload_btn.click()
