d2089f8aa888d78c159fc5a4bb40db2a0dcaf26c
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-05-22 19:37:01 +0530

    switching windows

--- SeleniumTutorials/basics/wiki_search.py
+++ SeleniumTutorials/basics/wiki_search.py
@@ -5,7 +5,7 @@ Created on 21-May-2025
 '''
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# import time
+import time
 
 '''1. Launching the chrome browser'''
 options = webdriver.ChromeOptions()
@@ -25,9 +25,31 @@ wiki_search_input.send_keys("Selenium")
 wiki_search_btn = driver.find_element(By.XPATH, '//*[@id="Wikipedia1_wikipedia-search-form"]/div/span[2]/span[2]/input')
 wiki_search_btn.click()
 
-# time.sleep(5)
+time.sleep(20)
 
 wiki_search_result = driver.find_element(By.XPATH, '(//*[@id="wikipedia-search-result-link"]/a)[1]')
 wiki_search_result.click()
 
+'''
+current_page_title = driver.title
+print(current_page_title)
+
+current_window = driver.current_window_handle
+print(current_window)
+# driver.switch_to.new_window('tab') # It opens new tab/ window
+'''
+window_handles_list = driver.window_handles
+# print(window_handles_list)
+
+driver.switch_to.window(window_handles_list[1])
+
+'''
+new_window = driver.current_window_handle
+print(new_window)
+
+new_page_title = driver.title
+print(new_page_title)
+'''
+history_link = driver.find_element(By.XPATH, '//*[@id="toc-History"]/a/div/span[2]')
+history_link.click()
 
