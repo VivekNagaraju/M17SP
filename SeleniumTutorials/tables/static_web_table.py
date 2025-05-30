5079c7e9f0fd7139457f58406176f4556cbd55e4
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-05-30 09:53:43 +0530

    Printing cell values separately

--- SeleniumTutorials/tables/static_web_table.py
+++ SeleniumTutorials/tables/static_web_table.py
@@ -20,13 +20,27 @@ driver.get('https://testautomationpractice.blogspot.com/')
 
 '''3. Print value from each cell'''
 
+
+
 cell_21 = driver.find_element(By.XPATH, '//*[@id="HTML1"]/div[1]/table/tbody/tr[2]/td[1]')
-cell_21_text = cell_21.text
-print(cell_21_text)
+cell_21_value = cell_21.text
+print(cell_21_value)
 
 cell_22 = driver.find_element(By.XPATH, '//*[@id="HTML1"]/div[1]/table/tbody/tr[2]/td[2]')
-cell_22_text = cell_22.text
-print(cell_22_text)
+cell_22_value = cell_22.text
+print(cell_22_value)
+
+cell_23 = driver.find_element(By.XPATH, '//*[@id="HTML1"]/div[1]/table/tbody/tr[2]/td[3]')
+cell_23_value = cell_23.text
+print(cell_23_value)
+
+cell_24 = driver.find_element(By.XPATH, '//*[@id="HTML1"]/div[1]/table/tbody/tr[2]/td[4]')
+cell_24_value = cell_24.text
+print(cell_24_value)
+
+cell_31 = driver.find_element(By.XPATH, '//*[@id="HTML1"]/div[1]/table/tbody/tr[3]/td[1]')
+cell_31_value = cell_31.text
+print(cell_31_value)
 
 '''
 //*[@id="HTML1"]/div[1]/table/tbody/tr[2]/td[2]
