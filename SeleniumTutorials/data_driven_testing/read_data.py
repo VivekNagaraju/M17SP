741dd0d1907bcd054fc3a3a44a93850a3c6f1c22
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-06-09 09:46:18 +0530

    Read data from specific sheet

--- SeleniumTutorials/data_driven_testing/read_data.py
+++ SeleniumTutorials/data_driven_testing/read_data.py
@@ -1,10 +1,28 @@
 import openpyxl
 
+
 filepath = "C:\\Users\\admin\\Desktop\\OrangeHRM_Login_DDT.xlsx"
 my_workbook = openpyxl.load_workbook(filepath)
-my_active_sheet = my_workbook.active
+# my_active_sheet = my_workbook.active # Get the currently active sheet
+my_active_sheet = my_workbook["Sheet1"] # Get the sheet by name
 total_rows = my_active_sheet.max_row
 print(total_rows)
 
 total_columns = my_active_sheet.max_column
 print(total_columns)
+
+username = my_active_sheet.cell(2, 1).value
+password = my_active_sheet.cell(2, 2).value
+print(username, password)
+
+username = my_active_sheet.cell(3, 1).value
+password = my_active_sheet.cell(3, 2).value
+print(username, password)
+
+username = my_active_sheet.cell(4, 1).value
+password = my_active_sheet.cell(4, 2).value
+print(username, password)
+
+username = my_active_sheet.cell(5, 1).value
+password = my_active_sheet.cell(5, 2).value
+print(username, password)
