4bb27e765190705be90d87c2fec1dfe98e911eb0
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-06-09 09:52:46 +0530

    Read data from table using for loop

--- SeleniumTutorials/data_driven_testing/read_data.py
+++ SeleniumTutorials/data_driven_testing/read_data.py
@@ -11,18 +11,8 @@ print(total_rows)
 total_columns = my_active_sheet.max_column
 print(total_columns)
 
-username = my_active_sheet.cell(2, 1).value
-password = my_active_sheet.cell(2, 2).value
-print(username, password)
+for i in range(2, total_rows+1):
+    username = my_active_sheet.cell(i, 1).value
+    password = my_active_sheet.cell(i, 2).value
+    print(username, password)
 
-username = my_active_sheet.cell(3, 1).value
-password = my_active_sheet.cell(3, 2).value
-print(username, password)
-
-username = my_active_sheet.cell(4, 1).value
-password = my_active_sheet.cell(4, 2).value
-print(username, password)
-
-username = my_active_sheet.cell(5, 1).value
-password = my_active_sheet.cell(5, 2).value
-print(username, password)
